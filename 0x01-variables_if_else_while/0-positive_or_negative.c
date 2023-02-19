37c4940a7eb186e06996519fa9d59bdca04a6f3b
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-19 22:06:33 +0000

    hello world

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include<stdlib.h>
 #include<time.h>
+/*more headers goes there */
 #include<stdio.h>
-
-/*
+/* betty style doc for function main goes there*/
+/**
 *main - Print sign of number
 * Return: 0
 */
