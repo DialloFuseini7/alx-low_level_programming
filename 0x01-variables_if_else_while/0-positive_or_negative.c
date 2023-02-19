2819da9963282bdcafebd5f4c176beeeb277902c
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-19 21:33:56 +0000

    hello world

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include<stdlib.h>
 #include<time.h>
 #include<stdio.h>
-/*betty style doc for function main goes there */
+/*betty style doc for function ma in goes there */
 
 int main(void)
 {
@@ -10,7 +10,7 @@ int main(void)
 	n = rand() -RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%i is positive\n",n);
+		printf("%i is positive\n", n);
 	}
 	else if (n < 0)
 	{
@@ -18,7 +18,7 @@ int main(void)
 	}
 	else 
 	{
-		printf("%i is zero\n". n);
+		printf("%i is zero\n", n);
 	}
 	return (0);
 }
