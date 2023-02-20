621f2dcf453d98f7a3a5994995e2daea541fcc45
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-20 14:24:47 +0000

    hello world

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-/*
+/**
  * main - Print sign of number
  * Return: 0
- * */
-
+ */
 int main(void)
 {
 	int i;
+	
 	for (i = 0; i < 26; i++)
 	{
-		putchar('a' +i);
+		putchar('a' + i);
 	}
 	for (i = 0; i < 26; i++)
 	{
