16ce7a287d55a7cb2fe398ce48a307129d241d94
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-20 15:22:57 +0000

    hello world

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-/*
+/**
  * main - Print sign of number
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
 	
-	for (i = 0; i <26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar('z' - i);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
