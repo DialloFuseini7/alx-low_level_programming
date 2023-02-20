88734201c0f082c8dd6268028562357aa8f0096f
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-20 15:18:53 +0000

    hello world

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-/*
+/**
  * main - Print sign of number
  * Return: 0
  */
-
 int main(void)
 {
 	int i;
-	for(i = 0; i < 10; i++)
+	
+	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
