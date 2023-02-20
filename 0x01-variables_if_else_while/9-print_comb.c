359b38b11c32b02d2b600eb43da794a08a282256
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-20 15:49:22 +0000

    hello world

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-/*
+/**
  * main - Print sign of number
  * Return: 0
  */
-
-int main(void)
+ int main(void)
 {
-       	int i;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
-		if(i != 9)
+		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
@@ -21,5 +20,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-
 }
