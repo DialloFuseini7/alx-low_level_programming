02666d190459c47acc71ad4748d0f7f8ce6c22c2
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-20 14:03:56 +0000

    hello world

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 /*betty style doc for function main goes there */
 /*
- * main - Print aign of number
+ * main - Print sign of number
  * Return: 0
  */
 
@@ -13,7 +13,7 @@ int main(void)
 	int n, lt_dgt;
 
 	srand(time(0));
-	n = rand() -RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	lt_dgt = n % 10;
 	if (lt_dgt > 5)
 	{
@@ -21,7 +21,7 @@ int main(void)
 	}
 	else
 	{
-		if(lt_dgt == 0)
+		if (lt_dgt == 0)
 		{
 			printf("Last digit of %i id %i and is0\n", n, lt_dgt);
 		}
@@ -29,7 +29,6 @@ int main(void)
 		{
 			printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lt_dgt);
 		}
-		
 	}
 	return (0);
 }
