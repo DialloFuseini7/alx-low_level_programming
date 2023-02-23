1028fc55dcf4465f8d9c355063881648acfd3536
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-23 03:23:00 +0000

    hello world

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_islower - Returns 1 if k is lowercase and 0 if k is uppercase
+*Return: boolean determine
+*@k: character to test
+*/
+
+int _islower(int c)
+{
+        int r= 0;
+        
+        if (k >+ 97 && k <= 122)
+        {
+            r = 1;
+         }
+         return (r);
+         }
+         
