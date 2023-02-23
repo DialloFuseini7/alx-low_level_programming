d3751aa4a159f3246b670497c7794def200712a1
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-23 03:07:10 +0000

    hello world

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,10 @@
  * main - prints putchar
  * Return: is always (0)
  */
-void print_alphabet(void);
-
 int main()
 {
     int a;
+    
     for (a = 'a'; a <= 'z'; a++)
     {
         putchar(a);
