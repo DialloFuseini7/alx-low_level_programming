a830484e32b03e421aec9fd885f9383002c9d002
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-23 02:15:13 +0000

    hello world

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - prints putchar
+ * Return: is always (0)
+ */
+void print_putchar(char *s);
+
+int main()
+{
+    // using char s[]
+    char s[] = "_putchar";
+    print_putchar(s);
+    return (0);
+}
+void print_putchar(char *s)
+{
+            int i = 0;
+            
+            while (s[i] != '\0')
+            {
+                putchar(s[i]);
+            i++;
+            }
+    putchar('\n');
+}
