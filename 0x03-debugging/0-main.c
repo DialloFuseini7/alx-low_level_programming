1292421f651fcf8d38765b665f901865b8d2282a
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-24 13:35:04 +0000

    hello world

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - tests function that prints if integer is positive or negative
  * positive_or_negative: defines if i is positive or negative
@@ -12,10 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> 42201b4c3099e5f38a4cc40427fc2943150b580b
 	return (0);
 }
