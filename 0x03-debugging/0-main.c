857a70cb156c933dc24bb81fa14cf3374ae1eb74
DialloFuseini7 <fuscudiallo@gmail.com>
2023-02-24 13:29:03 +0000

    hello world

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
+ * positive_or_negative: defines if i is positive or negative
  * Return: 0
  */
 
@@ -11,5 +12,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
-i}
+}
