4ed8bbae42b482df8dcff922d768da2570d0f5f9
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-24 00:41:37 +0000

    hello world

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *prints_alphabets - prints alphabets a -z
+ *print_alphabets - prints alphabets a -z
  * Return: always 0
  */
 int print_alphabets(void)
