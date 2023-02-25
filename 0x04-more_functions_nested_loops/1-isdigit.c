667a0a3c4b28a4551f02ce47a7748d4f6b64cc85
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-25 13:28:08 +0000

    hello world

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/** 
+* _isdigit - Returns 1 if c is a digit and 0 otherwise
+* Return: boolean determining if character is a digit
+* @c: character to test
+*/
+
+int _isdigit(int c)
+{
+        int i = 0;
+  
+        if (c >= 48 && c <= 57)
+        {
+            i = 1;
+        }
+  return (i);
+}
