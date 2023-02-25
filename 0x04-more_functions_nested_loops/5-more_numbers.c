e51e7b7a8e4f02c6d5adaa8f8040c43cc14500f1
DialloFuseini7 <107271037+DialloFuseini7@users.noreply.github.com>
2023-02-25 11:05:28 +0000

    hello world

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * more_numbers - prints numbers from 0 to 14
  */
@@ -8,9 +7,9 @@ void more_numbers(void)
 {
 	int i, a, b;
 		
-		for (i = 0; i <= 10; i++)
+		for (i = 0; i < 10; i++)
 		{
-			for (a = 0; a <= 14; a++)
+			for (a = 0; a < 15; a++)
 			{
 				b = i;
 
