6086c25cda179c78ff1198e28f94bc75ae5735a6
DialloFuseini7 <fuscudiallo@gmail.com>
2023-03-16 15:19:33 +0000

    hello world

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -25,16 +25,16 @@ int main(int argc, char *argv[])
 			unsigned long j;
 
 			for (j = 0; j < strlen(argv[i]); j++)
-					{
-					if (argv[i][j] < '0' || argv[i][j] > '9')
-					{
-					puts("Error");
-					return (1);
-					}
-					}
-					agg += atoi(*(argv + i));
-					}
-					printf("%d\n", agg);
-				}
-				return (0);
+			{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				puts("Error");
+				return (1);
+			}
+			}
+			agg += atoi(*(argv + i));
+		}
+		printf("%d\n", agg);
+	}
+	return (0);
 }
